c120c81074726025143d282b7c0dd184bdfa31a6
giuseppemaisto <gmaisto98@gmail.com>
2022-12-20 21:07:29 +0100

    inserito container in html

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const {
+    createApp
+} = Vue;
+
+createApp({
+    data() {
+        return {
+            checkTask: '',
+            todolist:[
+                {
+                    text:'fare la spesa',
+                    done: false,
+
+                },
+                {
+                    text:'fare la spesa',
+                    done: false,
+
+                },
+                {
+                    text:'fare la spesa',
+                    done: false,
+
+                },
+                {
+                    text:'fare la spesa',
+                    done: false,
+
+                },
+                {
+                    text:'fare la spesa',
+                    done: false,
+
+                },
+                
+            ]
+        }
+    },
+    methods: {
+        
+    },
+
+})
